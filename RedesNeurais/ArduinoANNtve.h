9c619c1bc79200283b05fd7e9b8bd6e5f9448d11
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2023-12-09 23:11:06 +0300

    Подобрался к двумерным в функцию как одномерным

---
+++ RedesNeurais/ArduinoANNtve.h
@@ -0,0 +1,82 @@
+/*
+  ArduinoANNtve.h - Библиотека для --------- мигания азбуки Морзе.
+  ---Created by David A. Mellis, November 2, 2007.
+  --Released into the public domain.
+*/
+#ifndef ArduinoANNtve_h
+#define ArduinoANNtve_h
+
+#include "Arduino.h"
+
+class ArduinoANN
+{
+  public:
+  
+  //ArduinoANN();
+  //ArduinoANN(byte _Input[], byte _Target[], int _PatternCount, int _InputNodes, int _OutputNodes);
+  ArduinoANN(int _PatternCount, int _InputNodes, int _OutputNodes, int _RowNol, int _ColNol, byte* aArray);
+  void Train();
+
+  static const int HiddenNodes = 3;           // Скрытые узлы - количество скрытых нейронов. 
+  static const float LearningRate = 0.3;      // Скорость обучения - параметр, который определяет, какая часть ошибки фактически передается обратно. 
+  static const float Momentum = 0.9;          // Импульс - регулирует, насколько результаты предыдущей итерации влияют на текущую итерацию.
+  static const float InitialWeightMax = 0.5;  // Начальный максимальный вес - устанавливает максимальные начальные значения для весов.
+  static const float Success = 0.0004;        // Успех - конечное значение порога ошибки, при котором будет считаться, что сеть решила обучающий набор.
+  
+  static const int PatternCount=4;            // Количество шаблонов - количество обучающих элементов или строк в таблице истинности. 
+  static const int InputNodes=2;              // Входные узлы - количество входных нейронов. 
+  static const int OutputNodes = 4;           // Выходные узлы - количество выходных нейронов.
+
+   int RowNol;
+   int ColNol;
+   //byte* myArray;
+
+
+  
+  /*
+  static const int PatternCount;              // Количество шаблонов - количество обучающих элементов или строк в таблице истинности. 
+  static const int InputNodes;                // Входные узлы - количество входных нейронов. 
+  static const int OutputNodes;               // Выходные узлы - количество выходных нейронов.
+  */
+  
+  byte Input[PatternCount][InputNodes]= 
+  {
+    {0,0},
+    {0,1},
+    {1,0},
+    {1,1}
+  }; 
+
+  byte Target[PatternCount][OutputNodes] = 
+  {
+    { 0, 0, 0, 1 },  
+    { 0, 0, 1, 0 }, 
+    { 0, 1, 0, 0 }, 
+    { 1, 0, 0, 0 }
+  };
+
+  private:
+  
+int i, j, p, q, r;
+int ReportEvery1000;
+int RandomizedIndex[PatternCount];
+long  TrainingCycle;
+float Rando;
+float Error;
+float Accum;
+
+float Hidden[HiddenNodes];
+float Output[OutputNodes];
+float HiddenWeights[InputNodes+1][HiddenNodes];         // Веса, поступающие в промежуточный слой  
+float OutputWeights[HiddenNodes+1][OutputNodes];        // Веса, поступающие на выход
+float HiddenDelta[HiddenNodes];
+float OutputDelta[OutputNodes];
+float ChangeHiddenWeights[InputNodes+1][HiddenNodes];   // Изменения обратного распространения из скрытого слоя
+float ChangeOutputWeights[HiddenNodes+1][OutputNodes];  // Изменения обратного распространения с выходов в скрытый слой
+
+void toTerminal();
+
+
+};
+
+#endif
